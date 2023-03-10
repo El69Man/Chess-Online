d3ef104f577ca5ecc8d16fa9f37377fafce15afd
El69Man <adrizombi12@gmail.com>
2023-03-10 17:04:16 +0100

    avanzando login

--- sprint1/signup.php
+++ sprint1/signup.php
@@ -1,9 +1,36 @@
+<!DOCTYPE html>
 <html>
 <body>
 
-<?PHP echo $_POST["username"]; ?><br>
-<?php echo $_POST["pwd"]; ?>
-<?php echo "Hello world!<br>";?>
+<?php
+include_once "connexio.php";
+$connex = mysqli_connect($lloc, $usuari, $pwd, $bbdd);
+if($connex){
+
+$consulta = "SELECT username, password from usuario";    
+$name = $_REQUEST['user'];
+$password = $_REQUEST["pwd"];
+
+if($resultat = mysqli_query($connex, $consulta)){
+    while($fila = mysqli_fetch_assoc($resultat)){
+        if($resultat = $name){
+            echo "existo";
+        }
+        else{
+            echo "no existo";
+        }
+    }
+}
+
+
+
+
+
+
+
+}
+
+?>
 
 </body>
 </html>
