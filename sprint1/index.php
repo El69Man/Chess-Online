075282de23a044317216e86049fab737be781b6a
El69Man <adrizombi12@gmail.com>
2023-03-22 20:43:33 +0100

    Rediseñado nav para imagen, perfil visible

--- sprint1/index.php
+++ sprint1/index.php
@@ -22,7 +22,7 @@
                 if (isset($_SESSION["username"])) {
                 ?>
                     <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#"><span class="glyphicon glyphicon-user"></span>Hola <?php echo $_SESSION["username"]?></a></li>
+                        <li><a href="perfil.php?username=<?php echo $_SESSION["username"]?>"><span class="glyphicon glyphicon-user"></span>Hola <?php echo $_SESSION["username"]?></a></li>
                         <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
                     </ul>
                 <?php
