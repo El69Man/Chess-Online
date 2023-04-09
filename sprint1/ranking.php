96422002292c8d17a6af8b39a83c73bff89e2161
El69Man <adri69man@gmail.com>
2023-04-09 16:45:24 +0200

    avanzando la tabla de ranking

--- sprint1/ranking.php
+++ sprint1/ranking.php
@@ -12,7 +12,6 @@
     #profilepic {
         max-width: 20%;
 		height: auto;
-        
     }
     </style>
 </head>
@@ -35,6 +34,12 @@
     //in this variable we took all data from the query
     $user_data = mysqli_fetch_assoc($result);
     }
+    //Aqui creem la llista de tots els usuaris ordenats per elo, 
+    $listar_usuarios = mysqli_query($connex, "SELECT TOP 100 * FROM usuario ORDER BY elo ASC");
+    $usuario = mysqli_fetch_assoc($listar_usuarios);
+
+    //Variable para limpiar codigo
+    $perfil_usuario = '<a href="perfil.php?username=<?php echo $usuario["username"]?>" style="padding: 0px;padding-right: 10px;"><span class="glyphicon"></span><img style="max-width: 50px;margin-right: 10px;" src="<?php echo $usuario["imagen"]?>"><?php echo $usuario["username"]?></a>';
 ?>
  <div id="container">
     <nav class="navbar navbar-inverse">
@@ -69,17 +74,26 @@
     </nav>
     <div class="row">
         <div id="ranking">
-
+            <table>
+                <tr>
+                    <th>Position</th>
+                    <th>User</th>
+                    <th>ELO</th>
+                </tr>
+                <?php
+                $suma =1;
+                    while ($usuario = mysqli_fetch_assoc($listar_usuarios)) {
+                        echo"<tr><td>".$suma."</td>";
+                        echo "<td>". $perfil_usuario."</td>";
+                        echo "<td>".$usuario["elo"]."</td></tr>";
+                        $suma = $suma +1;
+                    }
+                ?>
+            </table>
         </div>
     </div>
 </div>
-
-
-
-
-
 <?php
-
 //Close the SQL connection
   mysqli_close($connex);
 ?>
