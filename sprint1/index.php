11139f56546728d0370585f4fa2e669926d74c19
El69Man <adrizombi12@gmail.com>
2023-05-04 16:09:00 +0200

    temp

--- sprint1/index.php
+++ sprint1/index.php
@@ -6,7 +6,7 @@
     <script src="https://kit.fontawesome.com/0d820d26d5.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="chessboardJS/js/chessboard-1.0.0.js"></script>
-    <script src="index.js"></script>
+    <!--<script src="index.js"></script>-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="main.css">
     <script>
