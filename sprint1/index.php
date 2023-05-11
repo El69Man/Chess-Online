3bbb2d74d2370236dc888f583220daf8084a718e
El69Man <adrizombi12@gmail.com>
2023-05-11 20:29:38 +0200

    importado chess.js

--- sprint1/index.php
+++ sprint1/index.php
@@ -6,6 +6,7 @@
     <script src="https://kit.fontawesome.com/0d820d26d5.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="chessboardJS/js/chessboard-1.0.0.js"></script>
+    
     <script src="index.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
@@ -63,7 +64,18 @@ session_start();
             </div>
           </nav>
           <div class="rightButtons">
-            <button id="playBtn">Jugar</button>
+            <?php
+              if (isset($_SESSION["username"])) {
+              ?>
+                <button id="playBtn" class="valid">Jugar</button>
+            <?php
+                } 
+            else {
+            ?>
+                <button id="playBtn">Jugar</button>
+            <?php
+              }
+            ?>
             <div id="playLog">1.e4 e5</div>
           </div>
         <div class="container">
